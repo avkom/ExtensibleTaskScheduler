1c733b3ee81c8065c74f14c77b8306fd4336ee01
Oleksandr <avkomq@gmail.com>
2018-12-21 18:07:51 -0500

    Implement TracingTaskSchedulerExtension class

---
+++ src/Threading.Contrib/TracingTaskSchedulerExtension.cs
@@ -0,0 +1,24 @@
+﻿using OpenTracing;
+using OpenTracing.Util;
+
+namespace Threading.Contrib
+{
+    public class TracingTaskSchedulerExtension : ITaskSchedulerExtension
+    {
+        public object GetContextFromParentThread()
+        {
+            return GlobalTracer.Instance.ScopeManager.Active.Span;
+        }
+
+        public object EnterChildThread(object contextFromParentTread)
+        {
+            ISpan span = (ISpan) contextFromParentTread;
+            GlobalTracer.Instance.ScopeManager.Activate(span, false);
+            return null;
+        }
+
+        public void ExitChildThread(object contextFromParentThread, object contextFromChildThread)
+        {
+        }
+    }
+}
